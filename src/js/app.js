9bb709e85cb1566313564db54412a09ce2e3b6b3
dario1995 <dario.gagliano@outlook.com>
2017-06-19 12:47:37 +0200

    Update app.js
    commento nella funzione modif

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,12 @@ function hello(){
 	console.log("hello")
 	//commento!
 	/*secondo commento!*/
-<<<<<<< HEAD
 	// terzo commento!
 }
-=======
 }
 
 function modif(){
 	console.log("modifiche!");
+	//commento nella funzione modif
 }
->>>>>>> 32432139bfbf8cc46edbab5df76f10602fe6f3c7
+
